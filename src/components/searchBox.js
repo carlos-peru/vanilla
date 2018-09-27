332e09c1ee41485b73cb6ab2a2e454a7d9dcbd75
Carlos Castro Vargas <carlos.castro.vargas@gmail.com>
2018-09-27 14:30:02 +0300

    Wire up search into view

--- src/components/searchBox.js
+++ src/components/searchBox.js
@@ -1,9 +1,24 @@
 import "../styles/searchBox.css";
+import recipeService from "../services/recipeService";
+import renderRecipeList from "./recipeList";
+
+var recipeSearchResultsTimeoutId;
+
+const displayResults = () => {
+  const query = document.querySelector(".searchBox").value;
+  clearTimeout(recipeSearchResultsTimeoutId);
+  recipeSearchResultsTimeoutId = setTimeout(() => searchRecipes(query), 500);
+};
+
+const searchRecipes = query => {
+  recipeService.searchRecipes(query).then(results => renderRecipeList(results));
+};
 
 export default () => {
   const searchBox = document.createElement("input");
   searchBox.setAttribute("class", "searchBox");
   searchBox.setAttribute("type", "search");
   searchBox.setAttribute("placeholder", "Search term");
+  searchBox.addEventListener("input", displayResults);
   document.getElementById("search").appendChild(searchBox);
 };
