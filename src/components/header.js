6908867d39f9ce5bd96d34b1c8f83c80839d67c9
Carlos Castro Vargas <carlos.castro.vargas@gmail.com>
2018-09-28 11:05:21 +0300

    Update logo and remove menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,6 @@
 import "../styles/header.css";
 
 export default () => {
-  const header = `
-  <div>Recipe finer</div>
-  <nav>
-      <a href="#home">Home</a>
-  </nav>`;
+  const header = `<div>My Recipes</div>`;
   document.getElementsByTagName("header")[0].innerHTML = header;
 };
