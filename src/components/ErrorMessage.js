32cce006eafe08937d09b942459de96c464a54d7
Carlos Castro Vargas <carlos.castro.vargas@gmail.com>
2018-09-29 10:20:34 +0300

    Refactor the RecipeList and ErrorMessage to NOT handle display logic

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "../styles/ErrorMessage.css";
 
-export default display =>
-  display ? (
-    <div className="error">You can only enter alphanumeric values.</div>
-  ) : null;
+export default () => (
+  <div className="error">You can only enter alphanumeric values.</div>
+);
