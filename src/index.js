badfee7a397993a040b398f6f6192f587deacb88
Carlos Castro Vargas <carlos.castro.vargas@gmail.com>
2018-09-29 09:28:01 +0300

    Start App refactoring by adding an App component at the root level

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 import "./index.css";
-import renderHeader from "./components/header";
-import renderSearchBox from "./components/searchBox";
 
-renderHeader();
-renderSearchBox();
+ReactDOM.render(<App />, document.getElementById("app"));
